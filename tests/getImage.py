de67c3e3532a5b5903fe01d8e300fad6dc636a4f
Eduardo Dalcin <edalcin@jbrj.org>
2018-08-02 06:30:53 -0300

    Improving description

--- tests/getImage.py
+++ tests/getImage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+#Script para baixar as imagens do arquivo "image.txt"
+
 import urllib
 
 urllib.urlretrieve("http://imagens1.jbrj.gov.br/fsi/server?type=image&width=375&height=600&rect=0,0,1,1&profile=fsi&source=hvpr%2Fmnhn%2Ftipos4%2DParis%2FP00128420%2Ejpg&", "lauraceae1.jpg")
